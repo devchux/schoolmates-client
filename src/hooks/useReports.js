e3440fb365777cfb8308bb555a07e74b8f084bc8
Chukwudi Eze <chukwudieze97@gmail.com>
2023-01-13 11:24:50 +0100

    start reports feature

--- src/hooks/useReports.js
+++ src/hooks/useReports.js
@@ -1,7 +1,71 @@
 import { useState } from "react";
+import { useQuery } from "react-query";
+import queryKeys from "../utils/queryKeys";
+import { useAppContext } from "./useAppContext";
 
 export const useReports = () => {
+  const [enableIncomeQuery, setEnableIncomeQuery] = useState(false);
+  const [enableExpensesQuery, setEnableExpensesQuery] = useState(false);
+  const [openPrompt, setOpenPrompt] = useState(false);
   const [indexStatus, setIndexStatus] = useState('');
+  const [inputData, setInputData] = useState({
+    term: "",
+    session: "",
+  });
 
-  return { indexStatus, setIndexStatus };
+  const { term, session } = inputData;
+  const { apiServices, errorHandler } = useAppContext();
+
+  const togglePrompt = () => setOpenPrompt(!openPrompt);
+
+  const { isLoading: incomeReportsLoading, data: incomeReports } = useQuery(
+    [queryKeys.GET_ALL_INCOME_REPORTS, term, session],
+    () => apiServices.getAllIncomeReports(term, session),
+    {
+      enabled: enableIncomeQuery,
+      retry: 3,
+      onError(err) {
+        errorHandler(err);
+      },
+      onSuccess() {
+        setEnableIncomeQuery(false);
+        setOpenPrompt(false);
+        setIndexStatus('income');
+      },
+      select: apiServices.formatData,
+    }
+  );
+
+  const { isLoading: expensesReportsLoading, data: expensesReports } = useQuery(
+    [queryKeys.GET_ALL_EXPENSES_REPORTS, term, session],
+    () => apiServices.getAllExpenseReports(term, session),
+    {
+      enabled: enableExpensesQuery,
+      retry: 3,
+      onError(err) {
+        errorHandler(err);
+      },
+      onSuccess() {
+        setEnableExpensesQuery(false);
+        setOpenPrompt(false);
+        setIndexStatus('expense');
+      },
+      select: apiServices.formatData,
+    }
+  );
+
+  const isLoading = incomeReportsLoading || expensesReportsLoading;
+
+  return {
+    setEnableIncomeQuery,
+    setEnableExpensesQuery,
+    incomeReports,
+    isLoading,
+    setInputData,
+    expensesReports,
+    openPrompt,
+    togglePrompt,
+    indexStatus,
+    setIndexStatus,
+  };
 };
--- src/pages/dashboard/reports/index.jsx
+++ src/pages/dashboard/reports/index.jsx
@@ -1,9 +1,184 @@
-import React from 'react'
+import React from "react";
+import { useForm } from "react-formid";
+import AuthInput from "../../../components/inputs/auth-input";
+import AuthSelect from "../../../components/inputs/auth-select";
+import Prompt from "../../../components/modals/prompt";
+import PageView from "../../../components/views/table-view";
+import { useAppContext } from "../../../hooks/useAppContext";
+import { useReports } from "../../../hooks/useReports";
 
 const Reports = () => {
-  return (
-    <div>Reports</div>
-  )
-}
+  const {
+    apiServices: { handleSessionChange },
+  } = useAppContext();
+  const { inputs, handleSubmit, handleChange, errors, setFieldValue, reset } =
+    useForm({
+      defaultValues: { session: "", type: "income", term: "First Term" },
+      validation: {
+        session: {
+          required: true,
+        },
+        type: {
+          required: true,
+        },
+        term: {
+          required: true,
+        },
+      },
+    });
+  const {
+    setEnableIncomeQuery,
+    setEnableExpensesQuery,
+    incomeReports,
+    isLoading,
+    setInputData,
+    expensesReports,
+    openPrompt,
+    togglePrompt,
+    indexStatus,
+    setIndexStatus,
+  } = useReports();
 
-export default Reports
+  console.log(expensesReports, incomeReports);
+
+  const onSubmit = (data) => {
+    setInputData({
+      term: data.term,
+      session: data.session,
+    });
+    if (data.type === "income") {
+      setEnableIncomeQuery(true);
+    }
+
+    if (data.type === "expense") {
+      setEnableExpensesQuery(true);
+    }
+    reset();
+  };
+
+  const clear = () => setIndexStatus("");
+
+  const data = {
+    income: incomeReports,
+    expense: expensesReports,
+  };
+
+  const title = {
+    income: "Income Reports",
+    expense: "Expenses Reports",
+  };
+
+  const commonGroupButtonOptions = [
+    {
+      title: "Generate Report",
+      type: "button",
+      onClick: togglePrompt,
+      isLoading,
+    },
+  ];
+
+  return (
+    <div>
+      <PageView
+        hasSortOptions
+        pageTitle={title[indexStatus]}
+        showTableTitle={!!indexStatus}
+        showIllustration={!indexStatus}
+        hideTable={!indexStatus}
+        groupedButtonOptions={
+          indexStatus
+            ? [
+                ...commonGroupButtonOptions,
+                {
+                  title: "Clear",
+                  variant: "dark",
+                  type: "button",
+                  onClick: clear,
+                  isLoading,
+                },
+              ]
+            : commonGroupButtonOptions
+        }
+        canCreate={false}
+        isLoading={false}
+        columns={[
+          {
+            Header: "id",
+            accessor: "id",
+          },
+          {
+            Header: "Students",
+            accessor: "student_fullname",
+          },
+          {
+            Header: "Amount Paid",
+            accessor: "amount_paid",
+          },
+          {
+            Header: "Created At",
+            accessor: "created_at",
+          },
+        ]}
+        data={data[indexStatus]}
+      />
+      <Prompt
+        isOpen={openPrompt}
+        toggle={togglePrompt}
+        singleButtonProps={{
+          type: "button",
+          isLoading,
+          disabled: isLoading,
+          onClick: handleSubmit(onSubmit),
+        }}
+        singleButtonText="Continue"
+        promptHeader="Generate Report"
+      >
+        <div className="form-group mb-4">
+          <AuthSelect
+            label="Type"
+            value={inputs.type}
+            name="type"
+            hasError={!!errors.type}
+            onChange={handleChange}
+            options={[
+              { value: "income", title: "Income Report" },
+              { value: "expense", title: "Expenses Report" },
+            ]}
+          />
+          {!!errors.term && <p className="error-message">{errors.term}</p>}
+        </div>
+        <div className="form-group mb-4">
+          <AuthSelect
+            label="Term"
+            value={inputs.term}
+            name="term"
+            hasError={!!errors.term}
+            onChange={handleChange}
+            options={[
+              { value: "First Term", title: "First Term" },
+              { value: "Second Term", title: "Second Term" },
+              { value: "Third Term", title: "Third Term" },
+            ]}
+          />
+          {!!errors.term && <p className="error-message">{errors.term}</p>}
+        </div>
+        <div className="form-group mb-4">
+          <AuthInput
+            label="Session"
+            placeholder="2021/2022"
+            hasError={!!errors.session}
+            value={inputs.session}
+            onChange={({ target: { value } }) =>
+              handleSessionChange(value, "session", setFieldValue)
+            }
+          />
+          {!!errors.session && (
+            <p className="error-message">{errors.session}</p>
+          )}
+        </div>
+      </Prompt>
+    </div>
+  );
+};
+
+export default Reports;
