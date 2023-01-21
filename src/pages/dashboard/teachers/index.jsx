d6f8ca91fa4bdfd48a370d17b0e33ba67deccc9a
Chukwudi Eze <chukwudieze97@gmail.com>
2023-01-21 12:17:38 +0100

    temp fix

--- src/pages/dashboard/teachers/index.jsx
+++ src/pages/dashboard/teachers/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Spinner } from "reactstrap";
 // import Button from "../../../components/buttons/button";
-import TeacherProfileCard from "../../../components/cards/teacher-profile-card";
+import TeacherProfileCard from "../../../components/cards/Teacher-profile-card";
 import PageTitle from "../../../components/common/title";
 import { useHome } from "../../../hooks/useHome";
 
