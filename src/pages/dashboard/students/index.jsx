1b8eecc1a7e63cd3716eb9adcec75a2edc94c46f
Chukwudi Eze <chukwudieze97@gmail.com>
2023-01-24 00:16:45 +0100

    fix unused var

--- src/pages/dashboard/students/index.jsx
+++ src/pages/dashboard/students/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import PageView from "../../../components/views/table-view";
 import { useStudent } from "../../../hooks/useStudent";
 
